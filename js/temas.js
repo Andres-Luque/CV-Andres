ee89649ec4a0f1b32abc0d16ced94558e854270c
andres luque <andres.luque@gmail.com>
2023-02-21 21:34:10 -0300

    Modificacion en el responsive

---
+++ js/temas.js
@@ -0,0 +1,34 @@
+
+document.getElementById("btnTema1").addEventListener("click", fTemaUno);
+document.getElementById("btnTema2").addEventListener("click", fTemaDos);
+document.getElementById("btnRestablecer").addEventListener("click", fRestablecer);
+
+function fTemaUno() {
+
+    // for (let y = 0; y < 4; y++) {
+    //   document.getElementsByClassName("cDiv")[y].style.backgroundColor = "#EAC86C";
+    // }
+
+    document.body.style.backgroundColor = "#F0F392";
+
+}
+
+function fTemaDos() {
+
+    // for (let y = 0; y < 4; y++) {
+    //   document.getElementsByClassName("cDiv")[y].style.backgroundColor = "#EAC86C";
+    // }
+
+    document.body.style.backgroundColor = "#C0F392";
+
+}
+
+function fRestablecer() {
+
+    // for (let y = 0; y < 4; y++) {
+    //   document.getElementsByClassName("cDiv")[y].style.backgroundColor = "#EAC86C";
+    // }
+
+    document.body.style.backgroundColor = "antiquewhite";
+
+}
